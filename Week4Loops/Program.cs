186b6084e70362366937f2f09d8f8b1cea5d4689
Tony. <antony.poldre@tktk.ee>
2024-04-08 14:38:40 +0300

    add week4 code

---
+++ Week4Loops/Program.cs
@@ -0,0 +1,17 @@
+﻿
+//FOR-LOOP
+
+
+int i = 0;
+for(i = 0; i < 5; i++)
+{
+    Console.WriteLine("We dont need no education");
+}
+
+Console.WriteLine(i);
+Console.WriteLine("Have a nice day!");
+//Console.WriteLine("We dont need no education");
+//Console.WriteLine("We dont need no education");
+//Console.WriteLine("We dont need no education");
+//Console.WriteLine("We dont need no education");
+//Console.WriteLine("We dont need no education");
